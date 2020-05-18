9d6a83a37e4632c3cfdccf0a8affeb81300aff06
lu12345 <1295277401@qq.com>
2020-05-19 07:37:01 +0800

    all autotest file

---
+++ src/baidu_ui/const.py
@@ -0,0 +1,3 @@
+# coding = utf-8
+
+EXCEL_DIR="C:\\Python27\\pytestdemo\\data\\search_query.xlsx"
---
+++ src/httpbin_api/const.py
@@ -0,0 +1,5 @@
+# coding = utf-8
+BASE_URL = "http://httpbin.org/"
+IP_URL = "/ip"
+LOCAL_IP = "182.142.111.170"
+POST_TEST_URL = "/post"
